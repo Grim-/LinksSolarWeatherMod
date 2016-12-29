01e00ffab9d835701a252841b4f4b5de1b573b22
Bry <pheonix2105+github@gmail.com>
2016-12-29 19:15:29 +0000

    Added comments

--- Source/ExampleMod/ExampleMod/CompPowerPlantSolarWeather.cs
+++ Source/ExampleMod/ExampleMod/CompPowerPlantSolarWeather.cs
@@ -18,6 +18,7 @@ namespace LinksSolarMod
 
         private static readonly Material PowerPlantSolarBarUnfilledMat = SolidColorMaterials.SimpleSolidColorMaterial(new Color(0.15f, 0.15f, 0.15f));
 
+        //Here I create a new Red Material to use in the power display bar to show when its effected by weather
         private static readonly Material PowerPlantSolarBarUnderPoweredFilledMat = SolidColorMaterials.SimpleSolidColorMaterial(Color.red);
 
 
@@ -25,15 +26,21 @@ namespace LinksSolarMod
         {
             get
             {
+                //this is where the power output is decided notice 1700f (Max solar output) * (times) our weatherFactor
                 return Mathf.Lerp(0f, 1700f * this.weatherPowerFactor, this.parent.Map.skyManager.CurSkyGlow) * this.RoofedPowerOutputFactor;
             }
 
         }
 
+        //This is where the weatherPowerFactor is changed, we create a new float (private float weatherPowerFactor) and change its value depending on the weather.
         private float weatherPowerFactor
         {
             get
             {
+                //So if all you want to do is set power penalties depending on the weather add another "else if()" statement and set this.weatherPowerFactor to whatever you want the power factor to be.
+                //the power factor should be between 0 and 1.0 
+                //1700 power units * 0.5 = 850 power units
+                //conversly 1700 * 1.5 = 2550 ( a big power increase)
                 if (this.parent.Map.weatherManager.curWeather == WeatherDef.Named("Fog"))
                 {
                     return 0.2f;
@@ -42,6 +49,12 @@ namespace LinksSolarMod
                 {
                     return 0.001f;
                 }
+                else if(this.parent.Map.weatherManager.curWeather == WeatherDef.Named("Rain"))
+                {
+                    return 0.9f;
+                }
+
+
                 return 1.0f;
             }
         }
@@ -73,12 +86,19 @@ namespace LinksSolarMod
             r.size = CompPowerPlantSolarWeather.BarSize;
             r.fillPercent = base.PowerOutput / 1700f;
 
-            if(this.parent.Map.weatherManager.curWeather == WeatherDef.Named("Fog") || this.parent.Map.weatherManager.curWeather == WeatherDef.Named("SnowHard"))
+     
+            var curWeather = this.parent.Map.weatherManager.curWeather;
+
+            //This line simply uses the red material we defined above if the current weather is either fog or snowhard
+            //you can add the other weathers here by using || (or operator) and adding another 
+            /// if(curWeather == WeatherDef.Named("Fog") || curWeather == WeatherDef.Named("SnowHard") || curWeather == WeatherDef.Named("Rain") || currWeather = WeatherDef.Named("Yours here");
+            if (curWeather == WeatherDef.Named("Fog") || curWeather == WeatherDef.Named("SnowHard") || curWeather == WeatherDef.Named("Rain"))
             {
                 r.filledMat = CompPowerPlantSolarWeather.PowerPlantSolarBarUnderPoweredFilledMat;
             }
             else
             {
+                //otherwise use the normal yellow one
                 r.filledMat = CompPowerPlantSolarWeather.PowerPlantSolarBarFilledMat;
             }
             r.unfilledMat = CompPowerPlantSolarWeather.PowerPlantSolarBarUnfilledMat;
